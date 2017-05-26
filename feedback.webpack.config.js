7ed282139011f51264f7a16e7aa3dc631b75b1ba
17717066234 <m17717066234@aliyun.com>
2017-05-26 15:33:49 +0800

    mod

---
+++ feedback.webpack.config.js
@@ -0,0 +1,138 @@
+let path = require('path'),
+    webpack = require('webpack'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin'),
+    extractCSS = new ExtractTextPlugin('css/[name].css'),
+    extractLESS = new ExtractTextPlugin('css/[name]-less.css'),
+    extractSASS = new ExtractTextPlugin('css/[name]-sass.css');
+
+const NODE_ENV = process.env.NODE_ENV,
+    ROOT_PATH = path.resolve(__dirname, ''),
+    CONTEXT_ROOT = NODE_ENV === 'production' ? 'feedback' : '',
+    CONFIG = {
+        'NODE_ENV': NODE_ENV,
+        'ROOT_PATH': ROOT_PATH,
+        'CONTEXT_ROOT': CONTEXT_ROOT,
+        'OUTPUT_PATH': path.resolve(ROOT_PATH, CONTEXT_ROOT),
+        'PUBLIC_PATH': (CONTEXT_ROOT == null || CONTEXT_ROOT.length == 0) ? '/' : '/' + CONTEXT_ROOT + '/',
+        'FEEDBACK_HTML': 'index.html'
+    };
+
+/*debugger*/
+require('colors');
+for (let i in CONFIG) {
+    console.info(i.yellow + " => ".blue + CONFIG[i].red);
+}
+;
+/*debugger...end*/
+
+module.exports = {
+    entry: {
+        common: ['vue', 'vue-router', 'vue-resource'],
+        feedback: './feedback.js'
+    },
+    output: {
+        filename: './js/[name].js',
+        path: CONFIG.OUTPUT_PATH,
+        publicPath: CONFIG.PUBLIC_PATH
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }, {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: extractCSS.extract(['css-loader', 'postcss-loader'])/*['style-loader', 'css-loader']*/
+            },
+            {
+                test: /\.less$/i,
+                use: extractLESS.extract(['css-loader', 'less-loader'])
+            },
+            {
+                test: /\.scss$/,
+                use: extractSASS.extract(['css-loader', 'sass-loader'])
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+                loader: 'file-loader',
+                query: {
+                    name: 'file/[name].[ext]?[hash]'
+                }
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?\S*)?$/,
+                loader: 'file-loader',
+                query: {
+                    name: 'img/[name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            Css: path.resolve(__dirname, 'src/css/'),
+            Js: path.resolve(__dirname, 'src/js/'),
+            Vue: path.resolve(__dirname, 'src/vue/'),
+
+            vue: 'vue/dist/vue.js'
+        }
+    },
+    externals: {
+        /*vue: 'vue'*/
+    }
+}
+;
+
+module.exports.plugins = (module.exports.plugins || []).concat(
+    new HtmlWebpackPlugin({
+        title: '调查问卷',
+        hash: true,
+        js: ["feedback", "common"],
+        excludeChunks: ["index"],
+        filename: CONFIG.FEEDBACK_HTML
+    }),
+    extractCSS,
+    extractLESS,
+    extractSASS
+);
+
+
+if (CONFIG.NODE_ENV === 'development') {
+    module.exports.devtool = '#eval-source-map';
+    module.exports.devServer = {
+        port: 7777
+    };
+    module.exports.devtool = false;
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('development'),
+            'process.env.PRODUCTION': JSON.stringify(false),
+            'process.env.BASE_PATH': JSON.stringify('http://localhost:8066/one')
+        })
+    ]);
+} else if (CONFIG.NODE_ENV === 'production') {
+    module.exports.devtool = false;
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production'),
+            'process.env.PRODUCTION': JSON.stringify(true),
+            'process.env.BASE_PATH': JSON.stringify('https://app.cifm.com/one')
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: module.exports.devtool && (module.exports.devtool.indexOf("sourcemap") >= 0 || module.exports.devtool.indexOf("source-map") >= 0),
+            beautify: false,
+            comments: false
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['common'].reverse(),
+            filename: 'js/[name].js'
+        })
+    ]);
+}
---
+++ src/vue/Feedback.vue
@@ -0,0 +1,211 @@
+<style>
+    .icon-user input {
+        background: url("../img/user.png") no-repeat;
+        padding: 2px 0 2px 36px;
+        background-size: 28px auto;
+        background-position: 0 center;
+    }
+
+    .icon-mobile input {
+        background: url("../img/mobile.png") no-repeat;
+        padding: 2px 0 2px 36px;
+        background-size: 28px auto;
+        background-position: 0 center;
+    }
+
+    .text-center {
+        text-align: center;
+    }
+
+    .bg-white {
+        background: white;
+    }
+
+    .transition-box {
+        transition: all 0.6s;
+        display: block;
+        text-align: center;
+        position: fixed;
+        top: 0;
+        left: 0;
+        background-color: #fff;
+        width: 100%;
+        height: 100%;
+    }
+
+    .transition-box img {
+        display: block;
+        margin: 0 auto;
+        width: 100%;
+        max-width: 400px;
+    }
+
+    .transition-box p {
+        display: block;
+        color: #fb7a52;
+        font-size: 24px;
+        font-weight: 900;
+    }
+
+    .hide {
+        transition: all 0.6s;
+        opacity: 0;
+    }
+
+    .grid-content {
+        border-radius: 4px;
+        min-height: 36px;
+    }
+
+    .mg-top-20 {
+        margin-top: 20px;
+    }
+
+    @media screen and (max-width: 380px) { .transition-box p { font-size: 18px } }
+</style>
+<template>
+    <div>
+        <el-row>
+
+            <el-col :xs="1" :sm="2" :md="3" :lg="4">
+                <div class="grid-content"></div>
+            </el-col>
+            <el-col :xs="22" :sm="20" :md="18" :lg="16">
+                <img src="../img/pudding.png" alt="" style="display: block;width: 90%;max-width: 400px;margin: 0 auto">
+            </el-col>
+            <el-col :xs="1" :sm="2" :md="3" :lg="4">
+                <div class="grid-content bg-white"></div>
+            </el-col>
+        </el-row>
+
+
+        <el-row :gutter="10" class="mg-top-20">
+            <el-col :xs="1" :sm="2" :md="3" :lg="4">
+                <div class="grid-content bg-white"></div>
+            </el-col>
+            <el-col :xs="22" :sm="20" :md="18" :lg="16">
+                <div class="grid-content">
+
+                    <el-input
+                            placeholder="请输入姓名"
+                            v-model="feedbackParam.name" class="icon-user">
+                    </el-input>
+
+                    <el-input
+                            placeholder="请输入座机"
+                            icon="el-icon-information"
+                            v-model="feedbackParam.contact" class="mg-top-20 icon-mobile">
+                    </el-input>
+
+                    <el-input
+                            type="textarea"
+                            :rows="4"
+                            placeholder="请输入建议"
+                            v-model="feedbackParam.suggest" class="mg-top-20">
+                    </el-input>
+                </div>
+            </el-col>
+            <el-col :xs="1" :sm="2" :md="3" :lg="4">
+                <div class="grid-content bg-white"></div>
+            </el-col>
+        </el-row>
+        <el-row class="mg-top-20">
+            <el-col :xs="1" :sm="2" :md="3" :lg="4">
+                <div class="grid-content bg-white"></div>
+            </el-col>
+            <el-col :xs="22" :sm="20" :md="18" :lg="16">
+                <div class="grid-content text-center">
+                    <el-button type="danger" @click="submitFeedback" style="width: 100%;background-color: #fb7a52;">
+                        填好了
+                    </el-button>
+                </div>
+            </el-col>
+            <el-col :xs="1" :sm="2" :md="3" :lg="4">
+                <div class="grid-content bg-white"></div>
+            </el-col>
+        </el-row>
+        <el-row :style="modalStyle">
+            <div :class="{ 'transition-box': show, 'transition-box hide': !show}">
+                <el-col :xs="1" :sm="2" :md="3" :lg="4">
+                    <div class="grid-content bg-white"></div>
+                </el-col>
+                <el-col :xs="22" :sm="20" :md="18" :lg="16"class="mg-top-20">
+                    <img src="../img/pudding2.png" alt="" >
+                    <p class="mg-top-20">感谢您提的宝贵意见，么么哒！</p>
+                    <el-button type="danger" @click="switchModal" style="width: 100%;background-color: #fb7a52;">关闭</el-button>
+                </el-col>
+                <el-col :xs="1" :sm="2" :md="3" :lg="4">
+                    <div class="grid-content bg-white"></div>
+                </el-col>
+            </div>
+        </el-row>
+
+
+    </div>
+</template>
+
+<script>
+    require("element-ui/lib/theme-default/index.css");
+
+    import Vue from "vue";
+    import {
+        Row, Col, Input, Button, Message, Tooltip
+    }
+        from "element-ui";
+
+    Vue.use(Row);
+    Vue.use(Col);
+    Vue.use(Input);
+    Vue.use(Button);
+    Vue.use(Tooltip);
+
+    export default {
+
+        data(){
+            return {
+                modalStyle:{'z-index': -999},
+                show: false,
+                submiting:false,
+                feedbackParam: {
+                    name: "",
+                    contact: "",
+                    suggest: ""
+                }
+            }
+        },
+        methods: {
+            switchModal(){
+                this.show = !this.show;
+                this.modalStyle['z-index'] = -1 * this.modalStyle['z-index'];
+            },
+            submitFeedback(){
+                if (this.feedbackParam.name === "") {
+                    Message({
+                        message: "敢问大侠尊敬大名？"
+                    });
+                } else if (!/[-\d]+/.test(this.feedbackParam.contact)) {
+                    Message({
+                        message: "给个正确的联系方式吧？"
+                    });
+                } else if (this.feedbackParam.suggest === "") {
+                    Message({
+                        message: "Unbelievable!~难道我们的程序是完美的？"
+                    });
+                } else {
+                    if(!this.submiting) {
+                        this.submiting = true;
+                        this.$http.jsonp("/web/sys/feedback/save", {params: this.feedbackParam}).then(function (res) {
+                            this.submiting = false;
+                            this.switchModal();
+                            let data = res.data;
+                            Message({
+                                message: data.success ? "提交成功" : data.message
+                            });
+                        }.bind(this));
+                    }
+                }
+            }
+        }, created() {
+        }
+    }
+</script>
